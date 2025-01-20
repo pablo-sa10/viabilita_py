2e5837bbe2ac6f01e403dab8d8894fd4e44baf2f
pablo-sa10 <pablomoura2016@gmail.com>
2025-01-20 07:53:33 -0300

    db

--- config/database.py
+++ config/database.py
@@ -20,4 +20,28 @@ class DatabaseConnection:
     def connect(self):
         """ ESTABELECE A CONEXAO COM O BANCO DE DADOS """
         try:
+            self.connection = mysql.connector.connect(
+                host = self.host,
+                port = self.port,
+                user = self.user,
+                password = self.password,
+                database = self.database,
+            )
+
+            if self.connection.is_connected():
+                print("Conexão com o banco de dados estabelecida!")
+
         except Error as e:
+            print(f"Erro ao conectar ao banco de dados: {e}")
+            self.connection = None
+
+    def disconnect(self):
+        """ FECHA A CONEXAO COM O BANCO DE DADOS """
+        if not self.connection or not self.connection.is_connected():
+            print("Sem conexao ativa com o banco de dados.")
+            self.connection.close()
+            print("Conexao com o banco de dados")
+
+    def execute_query(self, query, params = None):
+        """ EXECUTA UMA CONSULTA SQL """
+        if not self.connection
